3b43ca69b4c98ac890da25f6cb4d3c750aa69384
bhargavachenna <bhargava.cracker@gmail.com>
2019-11-30 17:07:20 -0800

    5 files modified, 5 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Bhargava is good';
+export class AppComponent {
+  title = 'Employee Management System';
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { HttpClientModule } from '@angular/common/http'; 
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { DataService } from './data.service';
+import { PolicyService } from './policy.service';
+import { PoliciesComponent } from './policies/policies.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule, FormsModule, HttpClientModule,
+  InMemoryWebApiModule.forRoot(DataService) ],
+  declarations: [ AppComponent, HelloComponent, PoliciesComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [DataService, PolicyService]
 })
 export class AppModule { }
