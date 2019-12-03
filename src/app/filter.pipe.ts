d7523d300d4f6401b42971b0a256a7a581f4a85d
bhargavachenna <bhargava.cracker@gmail.com>
2019-12-03 18:49:39 +0530

    8 files modified

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -4,15 +4,14 @@ import { Iemployee } from './iemployee';
 @Pipe({
   name: 'filter'
 })
-export class FilterPipe implements PipeTransform {
 
+export class FilterPipe implements PipeTransform {
   transform(value: Iemployee[], searchString: string): any[] {
     if(!value) return [];
     if(!searchString) return value;
     searchString = searchString.toLowerCase();
     return value.filter( it => {
-      return it.names.toLowerCase().includes(searchString);
+      return it.names.toLowerCase().substring(0,searchString.length) === searchString;
     } );
   }
-
 }
